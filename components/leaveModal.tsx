c5df847126ae218f569b13604858d770ca71e265
oangsa <sukruangkul.aongsa@gmail.com>
2023-05-12 19:25:07 +0700

    tryna fix error

--- components/leaveModal.tsx
+++ components/leaveModal.tsx
@@ -19,11 +19,11 @@ const LeaveModal: FC<leaveModal> = ({ name, studentId }) => {
     const router: NextRouter = useRouter()
 
     const [data, setData] = useState<leaveFrom>({
-        reason: "",
-        half: ""
+        reason: "เลือกเหตุผล",
+        half: "เลือกเวลา"
     })
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
         const { name, value } = e.target
         console.log(name, value)
         setData((prev) => ({...prev, [name]: value}))
@@ -55,6 +55,9 @@ const LeaveModal: FC<leaveModal> = ({ name, studentId }) => {
     }
 
     const submit = async () => {
+        console.log(data.reason, data.half)
+        if (data.reason === "เลือกเหตุผล" || data.half === "เลือกเวลา") return
+        console.log("HEHE")
         // const req = await postData()
 
         // if ( req.message !== "Success" ) return swalFunc()
@@ -79,7 +82,7 @@ const LeaveModal: FC<leaveModal> = ({ name, studentId }) => {
                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                                 ระบบลาโรงเรียน
                             </h3>
-                            <button type="button" onClick={close} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                            <button type="button" onClick={close} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                 <span className="sr-only">Close modal</span>
                             </button>
@@ -88,13 +91,13 @@ const LeaveModal: FC<leaveModal> = ({ name, studentId }) => {
                             <div className="grid gap-4 mb-4 sm:grid-cols-1">
                                 <div>
                                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                                    <input disabled value={name} onChange={handleChange} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="ประยุทธ์" required />
+                                    <input disabled value={name} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="ประยุทธ์" required />
                                 </div>
                                 <p></p>
                                 <div>
                                     <label htmlFor="reason" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Reason</label>
-                                    <select defaultValue={data.reason} id="reason" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <option selected disabled>เลือกเหตุผล</option>
+                                    <select onChange={handleChange} value={data.reason} name="reason" id="reason" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <option disabled>เลือกเหตุผล</option>
                                         <option value="TV">TV/Monitors</option>
                                         <option value="PC">PC</option>
                                         <option value="GA">Gaming/Console</option>
@@ -104,8 +107,8 @@ const LeaveModal: FC<leaveModal> = ({ name, studentId }) => {
                                 <p></p>
                                 <div>
                                     <label htmlFor="half" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Time</label>
-                                    <select id="half" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <option selected disabled>เลือกเวลา</option>
+                                    <select onChange={handleChange} value={data.half} name="half" id="half" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <option disabled>เลือกเวลา</option>
                                         <option value="TV">TV/Monitors</option>
                                         <option value="PC">PC</option>
                                         <option value="GA">Gaming/Console</option>
