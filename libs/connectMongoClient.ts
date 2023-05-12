fae1cdaaeeabdee3d89efda06f28c7f79ad29bb9
oangsa <sukruangkul.aongsa@gmail.com>
2023-05-12 19:04:37 +0700

    fix v3

--- libs/connectMongoClient.ts
+++ libs/connectMongoClient.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connectMongo() {
+    const url:any = process.env.DB_URL
     if (mongoose.connection.readyState === 1) return mongoose.connection.asPromise();
-    return await mongoose.connect(process.env.DB_URL);
+    return await mongoose.connect(url);
 }
